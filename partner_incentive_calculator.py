be95197551fa285cf8c8237723505642af7900be
Rajw255 <raj.kumar@wealthy.in>
2026-09-23 18:13:22 +0530

    Update partner_incentive_calculator.py

--- partner_incentive_calculator.py
+++ partner_incentive_calculator.py
@@ -67,8 +67,8 @@ tier = tier_for_total(int(total_activation))  # not shown anywhere in the UI
 
 h1, h2, h3, h4 = st.columns([1, 0.5, 0.9, 0.4])
 h1.markdown("<span style='font-size:12px;color:gray'>Slab</span>", unsafe_allow_html=True)
-h2.markdown("<span style='font-size:12px;color:gray'>Active</span>", unsafe_allow_html=True)
-h3.markdown("<span style='font-size:12px;color:gray'>Inactive</span>", unsafe_allow_html=True)
+h2.markdown("<span style='font-size:12px;color:gray'>Active Partner</span>", unsafe_allow_html=True)
+h3.markdown("<span style='font-size:12px;color:gray'>Inactive Partner</span>", unsafe_allow_html=True)
 h4.markdown("<span style='font-size:12px;color:gray'>Incentive</span>", unsafe_allow_html=True)
 
 slab_sum = 0
